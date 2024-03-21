30133cb5269d41ea6c8c235ae188297ef8a32939
romilsonlonan <romilsonlonan@gmail.com>
2024-03-21 08:11:40 -0300

    Criado templates base e home, url home, views e models

--- virtualstore/models.py
+++ virtualstore/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cliente(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    nome_completo = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200,null=True,blank=True)
+    data_on = models.DateField(auto_now_add=True)
+     
+    def __str__(self):
+        return self.nome_completo
+    
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    
+class Produto(models.Model):
+    titulo = models.CharField(max_length=200)
+    #garante que o valor armazenado seja um texto simplificado, 
+    #composto apenas de letras, números, hífens e sublinhados, sem espaços ou caracteres especiais.
+    slug = models.SlugField(unique=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to="produtos")
+    preco_marcado = models.PositiveIntegerField()
+    venda = models.PositiveIntegerField()
+    descricao = models.TextField()
+    garantia = models.CharField(max_length=300, null=True, blank=True)
+    retorno_devolucao = models.CharField(max_length=300,null=True, blank=True)
+    visualizacao = models.PositiveIntegerField(default=0)    
+    
+    def __str__(self):
+        return self.titulo
+    
+    
+class Carrinho(models.Model):
+    cliente = models.ForeignKey(Cliente,on_delete=models.SET_NULL,null=True,blank=True)
+    total = models.PositiveIntegerField(default=0)    
+    criado_em = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return "Carrinho" + str(self.id)
+
+
+class CarrinhoProduto(models.Model):
+    carrinho = models.ForeignKey(Carrinho,on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto,on_delete=models.CASCADE)    
+    avaliacao = models.PositiveIntegerField()
+    quantidade = models.PositiveIntegerField()
+    subtotal = models.PositiveIntegerField()
+#    update = models.DateTimeField(auto_now = True)
+#    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return "Carrinho" + str(self.carrinho.id) + "CarrinhoProduto: " + str(self.id)
+        
+PEDIDO_STATUS=(
+    ("Pedido Recebido", "Pedido Recebido"),
+    ("Pedido Processado", "Pedido Processado"),
+    ("Pedido a caminho", "Pedido a caminho"),
+    ("Pedido Completado", "Pedido Completado"),
+    ("Pedido Cancelado", "Pedido Cancelado"),
+)
+        
+class PedidoOrdem(models.Model):
+    carrinho = models.OneToOneField(Carrinho,on_delete=models.CASCADE)
+    ordenado_por = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=10)
+    email = models.EmailField(null=True,blank=True)
+    endereco_envio = models.CharField(max_length=200)
+    # Não visualizado pelo cliente (instancia criada na views.py)
+    subtotal = models.PositiveIntegerField()
+    desconto = models.PositiveIntegerField() 
+    total = models.PositiveIntegerField()    
+    pedido_status = models.CharField(max_length=50,choices=PEDIDO_STATUS)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "Pedido_Ordem:" + str(self.id) 
--- virtualstore/views.py
+++ virtualstore/views.py
@@ -1,7 +1,82 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
+from django.forms import BaseModelForm
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import View, TemplateView, CreateView, FormView
+from django.urls import reverse_lazy
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produto_list'] = Produto.objects.all().order_by("-id")
+        return context
+    
+class TodosProdutosView(TemplateView):
+    template_name = 'todosprodutos.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['todoscategorias'] = Categoria.objects.all()
+        return context
+    
+class ProdutoDetalheView(TemplateView):
+    template_name = 'produtodetalhe.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+  
+        return context
+     
+class AddCarrinhoView(TemplateView):
+    template_name = 'addprocarrinho.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+   
+class MeuCarrinhoView(TemplateView):
+    template_name = 'meucarrinho.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        carrinho_id = self.request.session.get('carrinho_id', None)
+       
+        return context   
+
+                
+class ManipularCarrinhoView(View):
+    def get(self, request, *args, **kwargs):
+   
+        return redirect("lojavirtual:meucarrinho")
+
+
+class LimparCarrinhoView(View):
+    def get(self, request, *args, **kwargs):
+        carrinho_id = request.session.get('carrinho_id', None)
+    
+        return redirect('lojavirtual:meucarrinho')   
+    
+class CheckoutView(CreateView):
+    template_name = 'processar.html'
+  
+        
+
+   
+
+
+class ClienteSairView(View):
+    def pegar (self,request):
+        logout(request)
+        return redirect("lojavirtual:home")
+    
+    
+class ClienteEntrarView(FormView):
+    template_name = 'clienteentrar.html'
+
+    
+        
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
+    
+class ContatoView(TemplateView):
+    template_name = 'contato.html'   
